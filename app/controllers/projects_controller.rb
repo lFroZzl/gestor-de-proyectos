e714e1a23dd8481d460cd92d1e4dedac16487442
FroZz <mafiondres12@gmail.com>
2021-06-30 18:44:42 -0700

    subiendo a github(todo)

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,37 @@
+class ProjectsController < ApplicationController
+
+  def index
+    @select_state = 'All'
+    if params[:state].present? && params[:state] != 'All'
+      @select_state = params[:state]
+      @projects = Project.where('state = ?', params[:state])
+    else
+      @projects = Project.all
+    end
+  end
+  
+  def new
+    @project = Project.new
+  end
+
+  def create
+    # Project.create(
+    # name: params[:project][:name]
+    # description: params[:project][:description]
+    # state: params[:project][:state]
+    # starts_on: params[:project][:starts_on]
+    # ends_on: params[:project][:ends_on]
+    # )
+
+    post = Project.new(project_params)
+    post.save
+
+    redirect_to root_path
+
+  end
+
+  def project_params
+    params.require(:project).permit(:name, :description, :state, :starts_on, :ends_on)
+  end
+
+end
