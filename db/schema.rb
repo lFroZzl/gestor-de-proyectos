cdbe3019de4b4d992ab7bd4f7be4587631882d3f
FroZz <mafiondres12@gmail.com>
2021-06-30 18:49:27 -0700

    subiendo heroku

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,17 @@
 
 ActiveRecord::Schema.define(version: 2021_06_30_233140) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.string "image_url"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "projects", force: :cascade do |t|
     t.string "name"
     t.text "description"
